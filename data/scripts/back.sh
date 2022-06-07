95b357ea03fb840f182c94447fbb49e99cf22797
Petro Mozil <mozil.petryk@gmail.com>
2022-06-07 07:24:32 +0300

    Fix scripts

--- data/scripts/back.sh
+++ data/scripts/back.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 killall swaybg;
-swaybg -i /home/petro/.local/share/backgrounds/$(ls /home/petro/.local/share/backgrounds/|shuf -n 1) -m fill;
+swaybg -i ~/.local/share/backgrounds/$(ls ~/.local/share/backgrounds/|shuf -n 1) -m fill;
