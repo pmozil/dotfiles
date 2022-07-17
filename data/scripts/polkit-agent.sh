a9b7ddf31b22c637f95d643ce6ff512ed7202fe7
Petro Mozil <mozil.petryk@gmail.com>
2022-07-17 13:03:35 +0300

    Update sway config, add gentoo package list

--- data/scripts/polkit-agent.sh
+++ data/scripts/polkit-agent.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-[ -f /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 ] && exec /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+[ -f /usr/libexec/polkit-gnome-authentication-agent-1 ] && exec /usr/libexec/polkit-gnome-authentication-agent-1 &
