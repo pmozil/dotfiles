88d103f817b1b2dfe468f71faf9a3c928e6ba91a
Petro Mozil <mozil.petryk@gmail.com>
2022-08-04 14:19:31 +0300

    Update waybar config formatting

--- deploy-scripts/autoreadme.sh
+++ deploy-scripts/autoreadme.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
-for f in $(ls data/backgrounds|sort); do echo \*\*$f\*\*; echo; echo \!\[\]\(data/backgrounds\/$f\); echo; done > backgrounds.md
+for f in $(ls data/backgrounds|sort); do
+    echo \*\*$f\*\*;
+    echo;
+    echo \!\[\]\(data/backgrounds\/$f\);
+    echo;
+done > backgrounds.md
