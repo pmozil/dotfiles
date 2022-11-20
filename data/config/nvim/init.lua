001a0b364e8c379604fa9d1c9aebe76ec00b83d0
Petro Mozil <mozil.petryk@gmail.com>
2022-11-20 14:03:29 +0200

    Add coc to nvim

--- data/config/nvim/init.lua
+++ data/config/nvim/init.lua
@@ -6,21 +6,16 @@ vim.call("plug#begin")
     Plug 'folke/tokyonight.nvim'
     Plug 'junegunn/goyo.vim'
     Plug('junegunn/fzf', {['do'] = vim.fn['fzf#install']})
-
-    Plug 'preservim/nerdtree' 
-    Plug 'Xuyuanp/nerdtree-git-plugin'
-    Plug 'tiagofumo/vim-nerdtree-syntax-highlight'
-
     Plug 'karb94/neoscroll.nvim'
-
     Plug 'chrisbra/unicode.vim'
+    Plug('neoclide/coc.nvim', {['branch'] = 'release'})
 vim.call("plug#end")
-
-
-cmd("source ~/.config/nvim/nvim_init.vim")
 
 require('neoscroll').setup()
 local t = {}
 t['<leader>k'] = { 'scroll', { '-vim.wo.scroll', 'true', '350', 'sine', [['cursorline']] } }
 t['<leader>j'] = { 'scroll', {  'vim.wo.scroll', 'true', '350', 'sine', [['cursorline']] } }
 require('neoscroll.config').set_mappings(t)
+
+cmd("source ~/.config/nvim/nvim_init.vim")
+cmd("source ~/.config/nvim/coc_config.vim")
