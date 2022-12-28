180ba55bba01be1422f14c1b9417301613bbd58f
Petro Mozil <mozil.petryk@gmail.com>
2022-12-28 10:16:33 +0200

    Fix nvim config

--- data/config/nvim/init.lua
+++ data/config/nvim/init.lua
@@ -2,7 +2,11 @@ local cmd = vim.cmd
 local Plug = vim.fn['plug#']
 
 vim.call("plug#begin")
-    Plug 'voldikss/vim-floaterm'
+    Plug 'nvim-lua/plenary.nvim'
+    Plug 'nvim-lualine/lualine.nvim'
+    Plug 'kyazdani42/nvim-web-devicons'
+    Plug('akinsho/toggleterm.nvim')
+    Plug('sakhnik/nvim-gdb', {['do'] = vim.fn['!./install.sh'] })
     Plug 'folke/tokyonight.nvim'
     Plug 'junegunn/goyo.vim'
     Plug('junegunn/fzf', {['do'] = vim.fn['fzf#install']})
@@ -16,6 +20,24 @@ local t = {}
 t['<leader>k'] = { 'scroll', { '-vim.wo.scroll', 'true', '350', 'sine', [['cursorline']] } }
 t['<leader>j'] = { 'scroll', {  'vim.wo.scroll', 'true', '350', 'sine', [['cursorline']] } }
 require('neoscroll.config').set_mappings(t)
+
+
+require("toggleterm").setup{
+    size = function(term)
+    if term.direction == "horizontal" then
+      return 20
+    elseif term.direction == "vertical" then
+      return vim.o.columns * 0.4
+    end
+    end,
+}
+
+require('lualine').setup{
+    options = {
+        theme = 'auto',
+        icons_enabled = false
+    }
+}
 
 cmd("source ~/.config/nvim/nvim_init.vim")
 cmd("source ~/.config/nvim/coc_config.vim")
