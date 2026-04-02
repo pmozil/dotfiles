2a12ba47f2b9168024ac2a37bd702f9dcec2be98
Petro Mozil <mozil.petryk@gmail.com>
2026-04-02 10:21:25 +0300

    Change terminal
    Signed-off-by: Petro Mozil <mozil.petryk@gmail.com>

--- data/nvim/plugins/init.lua
+++ data/nvim/plugins/init.lua
@@ -24,7 +24,41 @@ return {
         local servers = {
             html = {},
             zls = {},
-            clangd = {},
+            clangd = {
+              keys = {
+                { "<leader>ch", "<cmd>LspClangdSwitchSourceHeader<cr>", desc = "Switch Source/Header (C/C++)" },
+              },
+              root_markers = {
+                "compile_commands.json",
+                "compile_flags.txt",
+                "configure.ac", -- AutoTools
+                "Makefile",
+                "configure.ac",
+                "configure.in",
+                "config.h.in",
+                "meson.build",
+                "meson_options.txt",
+                "build.ninja",
+                ".git",
+              },
+              capabilities = {
+                offsetEncoding = { "utf-16" },
+              },
+              cmd = {
+                "clangd",
+                "--background-index",
+                "--clang-tidy",
+                "--header-insertion=iwyu",
+                "--completion-style=detailed",
+                "--function-arg-placeholders",
+                "--fallback-style=llvm",
+              },
+              init_options = {
+                usePlaceholders = true,
+                completeUnimported = true,
+                clangdFileStatus = true,
+              },
+            },
             rust_analyzer = {},
             bashls = {},
             pyright = {
--- data/nvim/plugins/latex.lua
+++ data/nvim/plugins/latex.lua
@@ -3,8 +3,17 @@ return {
     "lervag/vimtex",
     lazy = false,
     init = function()
-      vim.g.vimtex_view_method = "zathura"
       vim.g.vimtex_compiler_method = "latexmk"
+
+      -- vim.g.vimtex_view_method = "general"
+      -- vim.g.vimtex_view_general_viewer = "/usr/bin/atril"
+      -- vim.g.vimtex_view_general_options = "--synctex-forward @line:@col:@tex @pdf"
+
+      vim.g.vimtex_view_method = "general"
+      vim.g.vimtex_view_general_viewer = "atril"
+      vim.g.vimtex_view_general_options = "@pdf"
+
+      vim.g.vimtex_toc_autoclose = 0
     end,
   },
 }
